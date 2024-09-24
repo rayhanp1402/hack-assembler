8dc0477cefbad8499da8cd8510333e86f70e75ed
rayhanp1402 <rayhan.hanif14maulana@gmail.com>
2024-09-24 17:05:47 +0700

    Parser

---
+++ parser.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_LINE_SIZE 4096  // POSIX suggested
+#define MAX_ADDRESS_SIZE 6
+#define MAX_DEST_SIZE 4
+#define MAX_COMP_SIZE 8
+#define MAX_JUMP_SIZE 4
+
+typedef struct instruction {
+    char addr[MAX_ADDRESS_SIZE];
+    char dest[MAX_DEST_SIZE];
+    char comp[MAX_COMP_SIZE];
+    char jump[MAX_JUMP_SIZE];
+
+    bool isAddrEmpty;
+    bool isDestEmpty;
+    bool isCompEmpty;
+    bool isJumpEmpty;
+} Instruction;
+
+void concatenate_character(char* string, char character) {
+    int stringLength = strlen(string);
+    string[stringLength + 1] = '\0';
+    string[stringLength] = character;
+}
+
+Instruction* parse_line_to_instruction(char* line) {
+    Instruction* instruction = (Instruction*) malloc(sizeof(Instruction));
+    instruction->addr[0] = '\0';
+    instruction->dest[0] = '\0';
+    instruction->comp[0] = '\0';
+    instruction->jump[0] = '\0';
+    instruction->isAddrEmpty = false;
+    instruction->isDestEmpty = false;
+    instruction->isCompEmpty = false;
+    instruction->isJumpEmpty = false;
+
+    // Stores current C-Instruction before knowing if is it dest/comp/jmp
+    char tempString[MAX_DEST_SIZE + MAX_COMP_SIZE + MAX_JUMP_SIZE + 2] = "";
+
+    int lineLength = strlen(line);
+    bool isAInstruction = false;
+    bool includeJump = false;
+    for (int i = 0; i < lineLength; ++i) {
+        if (line[i] == '\n') break;
+
+        if (isspace(line[i])) continue;
+
+        if (i < lineLength - 1 && line[i] == '/' && line[i+1] == '/') {
+            break;  // Terminate when comment is found
+        }
+
+        if (line[i] == '@') {
+            isAInstruction = true;
+            continue;
+        }
+
+        if (isAInstruction) {
+            concatenate_character(instruction->addr, line[i]);
+            continue;
+        }
+
+        if (line[i] == '=') {
+            strcat(instruction->dest, tempString);
+            tempString[0] = '\0';
+        } else if (line[i] == ';') {
+            strcat(instruction->comp, tempString);
+            tempString[0] = '\0';
+            includeJump = true;
+        } else {
+            concatenate_character(tempString, line[i]);
+        }
+    }
+
+    if (!isAInstruction) {
+        if (includeJump) {
+            strcat(instruction->jump, tempString);
+        } else {
+            strcat(instruction->comp, tempString);
+        }
+    }
+
+    return instruction;
+}
